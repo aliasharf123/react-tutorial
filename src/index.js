e5a04093a84d51fcf4b84d59ebed4a374b4b7f8b
aliashraf123 <aliashrafali770@gmail.com>
2023-02-19 08:49:43 +0200

    ssssssss

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
-import Index from './Lectures/Advansed Topics/12-memo-useMemo-useCallback/Index';
+import Matraial from './projects/Advansed projects/project 2/matrial';
 
 
 function Final() {
-    return ( <Index/>);
+    return ( <Matraial/>);
 }
 const container = document.getElementById('root');
 const root = createRoot(container); 
